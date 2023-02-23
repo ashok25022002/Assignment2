d4b18443186e655480ed8688caacf981a5d943d9
ashok25022002 <126159832+ashok25022002@users.noreply.github.com>
2023-02-23 16:11:22 +0530

    Add files via upload

---
+++ sting5.java
@@ -0,0 +1,7 @@
+public class sting5 {
+    public static void main(String args[]){
+        String s1="java by gowtham";
+        System.out.println(s1.endsWith("t"));
+        System.out.println(s1.endsWith("am"));
+    }
+}
---
+++ string3.java
@@ -0,0 +1,11 @@
+public class string3 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = " Gowtham";
+
+        String str3 = str1.concat(str2);
+
+        System.out.println(str3);
+
+    }
+}
